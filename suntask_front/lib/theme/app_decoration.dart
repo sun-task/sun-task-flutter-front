2f5a0d26dc111c546753e0e44b60bb592b946a7d
SolFreire <114754733+SolFreire@users.noreply.github.com>
2023-05-28 11:21:52 -0300

    Update app_decoration.dart

--- suntask_front/lib/theme/app_decoration.dart
+++ suntask_front/lib/theme/app_decoration.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:sol_s_application1/core/app_export.dart';
+import 'package:suntask_front/core/app_export.dart';
 
 class AppDecoration {
   static BoxDecoration get outlineBlack9003f => BoxDecoration(
